1ff9905e33f43ad684fe62754e79e43976735ba6
MgAl2O4 <51430403+MgAl2O4@users.noreply.github.com>
2021-12-02 20:59:09 -0500

    fixed? metronome code signatures

--- dalamud/plugin/UnsafeMetronomeLink.cs
+++ dalamud/plugin/UnsafeMetronomeLink.cs
@@ -90,7 +90,7 @@ namespace HarpHero
                     ptrGetBPMFunc = sigScanner.ScanText("e8 ?? ?? ?? ?? f3 0f 10 4e 04 0f b7 c0 66 0f 6e c0");
                     ptrGetMeasureFunc = sigScanner.ScanText("e8 ?? ?? ?? ?? 0f be 56 08 0f b7 c0 3b c2 74 ?? 48");
 
-                    ptrMetronomeStopFunc = sigScanner.ScanText("40 53 48 83 EC 20 48 8B 01 48 8B D9 FF 50 20 84 C0 74 ?? 48 8B CB C6 43 73 00");
+                    ptrMetronomeStopFunc = sigScanner.ScanText("40 53 48 83 EC 20 48 8B 01 48 8B D9 FF 50 20 84 C0 74");
                     //PluginLog.Log($"stopFunc: +0x{((long)ptrMetronomeStopFunc - (long)sigScanner.Module.BaseAddress):X}");
                 }
                 catch (Exception)
